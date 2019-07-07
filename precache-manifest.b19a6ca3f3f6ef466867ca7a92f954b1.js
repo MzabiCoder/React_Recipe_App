f236a91d640b7eacea0bf4269b07ed94adf83ba7
Nabil Fannane <nabil@goadroit.com>
2019-07-06 22:17:26 -0700

    Updates

---
+++ precache-manifest.b19a6ca3f3f6ef466867ca7a92f954b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3c7f24f9649fd2011fe",
+    "url": "/React_Recipe_App/static/css/main.0bdf18ea.chunk.css"
+  },
+  {
+    "revision": "a3c7f24f9649fd2011fe",
+    "url": "/React_Recipe_App/static/js/main.a3c7f24f.chunk.js"
+  },
+  {
+    "revision": "a4f88a12d413e3612f35",
+    "url": "/React_Recipe_App/static/css/1.bdd7e14a.chunk.css"
+  },
+  {
+    "revision": "a4f88a12d413e3612f35",
+    "url": "/React_Recipe_App/static/js/1.a4f88a12.chunk.js"
+  },
+  {
+    "revision": "e6066460f029af65dde1",
+    "url": "/React_Recipe_App/static/js/runtime~main.e6066460.js"
+  },
+  {
+    "revision": "187f73b71f966299c9447a6ecfbff8ac",
+    "url": "/React_Recipe_App/index.html"
+  }
+];
